2c9436b1d39c49ab854e5dec8a1e312550bb405a
Lena Krasova <krasovaem@gmail.com>
2024-03-26 11:05:48 +0500

    HW1 in process 3 tests done

--- test_HW1.py
+++ test_HW1.py
@@ -1,26 +1,40 @@
+import time
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 
 
-browser = webdriver.Chrome()
+browser = webdriver.Firefox()
+######################## Авторизация ########################
 
-def test_auth_positive():
+# def test_auth_positive():
+#     browser.get('https://www.saucedemo.com')
+
+#     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
+#     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
+#     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+#     assert browser.current_url == 'https://www.saucedemo.com/inventory.html', 'url не соответствует ожидаемому'
+
+#     browser.quit()
+
+# def test_auth_negative():
+#     browser.get('https://www.saucedemo.com')
+#     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('user')
+#     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('user')
+#     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+#     assert browser.find_element(By.XPATH, '//h3[contains(@data-test, "error")]').is_displayed
+
+#     browser.quit()
+
+######################## Корзина ########################
+
+def test_add_item_to_cart():
     browser.get('https://www.saucedemo.com')
 
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
-    assert browser.current_url == 'https://www.saucedemo.com/inventory.html', 'url не соответствует ожидаемому'
+    time.sleep(2)
+    browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+    assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text == '1'
 
-
-    browser.quit()
-
-def test_auth_negative():
-    browser.get('https://www.saucedemo.com')
-
-    browser.find_element('xpath', '//*[@id="user-name"]').send_keys('user')
-    browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('user')
-    browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
-    assert browser.find_element(By.XPATH, '//h3[contains(@data-test, "error")]'), 'нужна другая проверка'
-    
     browser.quit()
--- test_it.py
+++ test_it.py
@@ -10,8 +10,8 @@ driver = webdriver.Chrome()
 
 def test_autorization():
     driver.get("https://www.saucedemo.com")
-    driver.find_element(By.NAME, "user-name").sendKeys("standard_user")
-    driver.find_element(By.NAME, "password").sendKeys("secret_sauce")
+    driver.find_element(By.NAME, "user-name").send_keys("standard_user")
+    driver.find_element(By.NAME, "password").send_keys("secret_sauce")
     driver.find_element(By.NAME, "login-button").click()
     assert driver.current_URL == "https://www.saucedemo.com/inventory.html"
     driver.quit()
