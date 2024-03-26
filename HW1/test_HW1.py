5ae7a541df16977681efd3f1fdc6e089f48bf9fc
Lena Krasova <krasovaem@gmail.com>
2024-03-26 18:10:13 +0500

    HW1

--- HW1/test_HW1.py
+++ HW1/test_HW1.py
@@ -17,6 +17,7 @@ def test_auth_positive():
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/inventory.html', 'url не соответствует ожидаемому'
 
     browser.quit()
@@ -28,6 +29,7 @@ def test_auth_negative():
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('user')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '//h3[contains(@data-test, "error")]').is_displayed
 
     browser.quit()
@@ -43,6 +45,7 @@ def test_add_item_to_cart():
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
     time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text == '1'
 
     browser.quit()
@@ -56,9 +59,12 @@ def test_delete_item_from_cart():
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
     time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+    time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/cart.html'
     browser.find_element(By.XPATH, '//*[@id="remove-sauce-labs-backpack"]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '//*[@id="cart_contents_container"]/div/div[1]/div[3]').text == ''
     
     browser.quit()
@@ -70,13 +76,13 @@ def test_add_delete_item_to_cart_from_card():
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
-    
+    time.sleep(2)
     browser.get('https://www.saucedemo.com/inventory-item.html?id=4')
-    time.sleep(2)
     browser.find_element(By.ID, "add-to-cart").click()
-    assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text == '1'
     time.sleep(2)
+    assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text == '1'
     browser.find_element(By.ID, "remove").click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').text == ''
 
     browser.quit()
@@ -123,9 +129,12 @@ def test_checkout():
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
     time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+    time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/cart.html'
     browser.find_element(By.XPATH, '//*[@id="checkout"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/checkout-step-one.html'
     first_name = faker_en.first_name_female()
     last_name = faker_en.last_name_female()
@@ -134,8 +143,10 @@ def test_checkout():
     browser.find_element('xpath', '//*[@id="last-name"]').send_keys(f'{last_name}')
     browser.find_element('xpath', '//*[@id="postal-code"]').send_keys(f'{postal_code}')
     browser.find_element(By.XPATH, '//*[@id="continue"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/checkout-step-two.html'
     browser.find_element(By.XPATH, '//*[@id="finish"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/checkout-complete.html'
     
     browser.quit()
@@ -149,18 +160,19 @@ def test_filter():
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[1]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_name "])[1]').text == 'Sauce Labs Backpack'
-    time.sleep(3)
     browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[2]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_name "])[1]').text == 'Test.allTheThings() T-Shirt (Red)'
-    time.sleep(3)
     browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[3]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_price"])[1]').text == '$7.99'
-    time.sleep(3)
     browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[4]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_price"])[1]').text == '$49.99'
-    time.sleep(3)
 
     browser.quit()
 
@@ -173,9 +185,12 @@ def test_menu_about():
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/inventory.html', 'url не соответствует ожидаемому'
     browser.find_element(By.XPATH, '//*[@id="react-burger-menu-btn"]').click()
+    time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="about_sidebar_link"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://saucelabs.com/', 'url не соответствует ожидаемому'
 
     browser.quit()
@@ -188,11 +203,15 @@ def test_menu_reset():
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/inventory.html', 'url не соответствует ожидаемому'
     browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a/span').text == '1'
     browser.find_element(By.XPATH, '//*[@id="react-burger-menu-btn"]').click()
+    time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="reset_sidebar_link"]').click()
+    time.sleep(2)
     assert browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').text == ''
 
     browser.quit()
@@ -204,9 +223,12 @@ def test_menu_logout():
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/inventory.html', 'url не соответствует ожидаемому'
     browser.find_element(By.XPATH, '//*[@id="react-burger-menu-btn"]').click()
+    time.sleep(2)
     browser.find_element(By.XPATH, '//*[@id="logout_sidebar_link"]').click()
+    time.sleep(2)
     assert browser.current_url == 'https://www.saucedemo.com/', 'url не соответствует ожидаемому'
 
     browser.quit()
