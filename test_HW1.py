5bbefe294720193f97b55b436ca7839f03e2f5f2
Lena Krasova <krasovaem@gmail.com>
2024-03-26 15:04:16 +0500

    HW1 in progress 12 tests completed

--- test_HW1.py
+++ test_HW1.py
@@ -106,27 +106,49 @@ Faker.seed()
 
 ######################## Оформление заказа ########################
 
-def test_checkout():
+# def test_checkout():
+#     browser.get('https://www.saucedemo.com')
+    
+#     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
+#     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
+#     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+#     time.sleep(2)
+#     browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+#     browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]').click()
+#     assert browser.current_url == 'https://www.saucedemo.com/cart.html'
+#     browser.find_element(By.XPATH, '//*[@id="checkout"]').click()
+#     assert browser.current_url == 'https://www.saucedemo.com/checkout-step-one.html'
+#     first_name = faker_en.first_name_female()
+#     last_name = faker_en.last_name_female()
+#     postal_code = faker_en.postalcode()
+#     browser.find_element('xpath', '//*[@id="first-name"]').send_keys(f'{first_name}')
+#     browser.find_element('xpath', '//*[@id="last-name"]').send_keys(f'{last_name}')
+#     browser.find_element('xpath', '//*[@id="postal-code"]').send_keys(f'{postal_code}')
+#     browser.find_element(By.XPATH, '//*[@id="continue"]').click()
+#     assert browser.current_url == 'https://www.saucedemo.com/checkout-step-two.html'
+#     browser.find_element(By.XPATH, '//*[@id="finish"]').click()
+#     assert browser.current_url == 'https://www.saucedemo.com/checkout-complete.html'
+    
+#     browser.quit()
+
+######################## Фильтр ########################
+    
+def test_filter():
     browser.get('https://www.saucedemo.com')
     
     browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
-    time.sleep(2)
-    browser.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
-    browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]').click()
-    assert browser.current_url == 'https://www.saucedemo.com/cart.html'
-    browser.find_element(By.XPATH, '//*[@id="checkout"]').click()
-    assert browser.current_url == 'https://www.saucedemo.com/checkout-step-one.html'
-    first_name = faker_en.first_name_female()
-    last_name = faker_en.last_name_female()
-    postal_code = faker_en.postalcode()
-    browser.find_element('xpath', '//*[@id="first-name"]').send_keys(f'{first_name}')
-    browser.find_element('xpath', '//*[@id="last-name"]').send_keys(f'{last_name}')
-    browser.find_element('xpath', '//*[@id="postal-code"]').send_keys(f'{postal_code}')
-    browser.find_element(By.XPATH, '//*[@id="continue"]').click()
-    assert browser.current_url == 'https://www.saucedemo.com/checkout-step-two.html'
-    browser.find_element(By.XPATH, '//*[@id="finish"]').click()
-    assert browser.current_url == 'https://www.saucedemo.com/checkout-complete.html'
-    
-    browser.quit()
+    browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[1]').click()
+    assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_name "])[1]').text == 'Sauce Labs Backpack'
+    time.sleep(3)
+    browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[2]').click()
+    assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_name "])[1]').text == 'Test.allTheThings() T-Shirt (Red)'
+    time.sleep(3)
+    browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[3]').click()
+    assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_price"])[1]').text == '$7.99'
+    time.sleep(3)
+    browser.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select/option[4]').click()
+    assert browser.find_element(By.XPATH, '(//*[@class="inventory_item_price"])[1]').text == '$49.99'
+    time.sleep(3)
+
